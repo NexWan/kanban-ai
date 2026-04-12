d71249eb88007e862a8daf4b661896b65fa71baf
NexWan <lcontrerasmartinez235@gmail.com>
2026-04-12 03:23:58 -0600

    feat: add board domain struct

---
+++ apps/api/internal/domain/board.go
@@ -0,0 +1,10 @@
+package domain
+
+type Board struct {
+	ID          string `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	CreatedAt   string `json:"created_at"`
+	UpdatedAt   string `json:"updated_at"`
+	AssignedTo  string `json:"assigned_to"`
+}
