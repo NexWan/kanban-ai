d67c702c2a2eca3dbddeee4fd017e03517164fb8
NexWan <lcontrerasmartinez235@gmail.com>
2026-04-12 05:09:07 -0600

    fix(Backend-Go): make data types match database definitions

--- apps/api/internal/domain/column.go
+++ apps/api/internal/domain/column.go
@@ -1,10 +1,12 @@
 package domain
 
-type Domain struct {
-	Id        string `json:"id"`
-	BoardId   string `json:"board_id"`
-	Name      string `json:"name"`
-	Position  int    `json:"position"`
-	CreatedAt string `json:"created_at"`
-	UpdatedAt string `json:"updated_at"`
+import "time"
+
+type Column struct {
+	Id        string    `json:"id"`
+	BoardId   string    `json:"board_id"`
+	Name      string    `json:"name"`
+	Position  int       `json:"position"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
