42f576faf40364c146dbeeb07da1f352bfffa2ba
NexWan <lcontrerasmartinez235@gmail.com>
2026-04-12 23:40:55 -0600

    feat(Backend-Go): add new routes handlers to the router

--- apps/api/internal/app/router.go
+++ apps/api/internal/app/router.go
@@ -19,12 +19,24 @@ func NewRouter(db *pgxpool.Pool) http.Handler {
 	// Define routes
 
 	r.Get("/health", handlers.HealthHandler)
+	// Board routes
 	r.Get("/boards", boardHandler.GetBoards)
+	r.Get("/boards/{id}", boardHandler.GetBoardByID)
 	r.Post("/boards", boardHandler.CreateBoard)
+	r.Put("/boards/{id}", boardHandler.UpdateBoard)
+	r.Delete("/boards/{id}", boardHandler.DeleteBoard)
+	// Card routes
 	r.Get("/cards", cardHandler.GetCards)
+	r.Get("/cards/{id}", cardHandler.GetCardByID)
 	r.Post("/cards", cardHandler.CreateCard)
+	r.Put("/cards/{id}", cardHandler.UpdateCard)
+	r.Delete("/cards/{id}", cardHandler.DeleteCard)
+	// Column routes
 	r.Get("/columns", columnHandler.GetColumns)
+	r.Get("/columns/{id}", columnHandler.GetColumnByID)
 	r.Post("/columns", columnHandler.CreateColumn)
+	r.Put("/columns/{id}", columnHandler.UpdateColumn)
+	r.Delete("/columns/{id}", columnHandler.DeleteColumn)
 
 	return r
 }
